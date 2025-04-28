73125c9f787ea2ce3380aeb2c437807bfbb29c2f
dxsvyazi <127469851+dxsvyazi@users.noreply.github.com>
2025-04-28 23:50:43 +0700

    Update OrderItem.tsx

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -1,34 +1,26 @@
 import { FC } from 'react';
 import { ProductWithAmount as OrderItemProps } from '@sharedTypes/productTypes';
 import { nameToPath } from '@utils/shared';
-import ProductInfo, {
-  Amount,
-  Name,
-  Price,
-  TotalPrice,
-} from './ProductInfo';
+import ProductInfo, { Amount, Name, Price, TotalPrice } from './ProductInfo';
 import Container from './base/Container';
 
-const OrderItem: FC<OrderItemProps> = ({ name, amount, price }) => {
+const OrderItem: FC<OrderItemProps> = (item) => {
   return (
-    <ProductInfo
-      name={name}
-      price={price}
-      amount={amount}
-      className='justify-between w-full'
-    >
+    <ProductInfo {...item} className='justify-between w-full'>
       <img
         className='size-12 rounded-xl self-center'
-        src={`./src/assets/products/${nameToPath(name)}/image-thumbnail.jpg`}
+        src={`./src/assets/products/${nameToPath(
+          item.name
+        )}/image-thumbnail.jpg`}
       />
       <Container className='mr-auto' variants={{ flow: 'col' }}>
         <Name className='truncate' />
         <Container className='p-0'>
           <Amount />
-          <Price prefix='@'/>
+          <Price prefix='@' />
         </Container>
       </Container>
-      <TotalPrice className='self-center'/>
+      <TotalPrice className='self-center' />
     </ProductInfo>
   );
 };
