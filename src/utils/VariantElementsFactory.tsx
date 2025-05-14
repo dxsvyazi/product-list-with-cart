3e7dfe3652d1b1464c2e4d74bdde7f043f6b089a
bebra <wintersxul@gmail.com>
2025-05-15 00:02:36 +0700

    Types fix

--- src/utils/VariantElementsFactory.tsx
+++ src/utils/VariantElementsFactory.tsx
@@ -1,21 +1,52 @@
-import { ElementType, FC, ComponentPropsWithRef as Props } from 'react';
-import { VariantProps } from 'class-variance-authority';
+import { ElementType, ComponentPropsWithRef as Props } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
 import { cn } from '@utils/shared';
 
-const ve = <T extends ElementType, U extends (...args: any) => any>(
+type VeFnType = ReturnType<typeof cva>;
+
+type VeProps<
+  T extends ElementType,
+  V extends ElementType,
+  U extends VeFnType
+> = {
+  variants?: VariantProps<U>;
+  className?: string;
+} & ResolvedProps<T, V>;
+
+type Resolved<T extends ElementType> = T extends TComponentType
+  ? TComponentType
+  : T;
+
+type ResolvedProps<
+  T extends ElementType,
+  V extends ElementType
+> = T extends TComponentType ? Props<TComponentTypeG<V>> : Props<T>;
+
+export type TComponentType = typeof TComponent;
+
+export type TComponentTypeG<T extends ElementType> = typeof TComponent<T>;
+
+export const TComponent = <T extends ElementType>({
+  as: Component = 'div' as T,
+  ...props
+}: { as?: T } & Omit<Props<T>, 'as'>) => <Component {...(props as Props<T>)} />;
+
+export const ve = <T extends ElementType, U extends VeFnType>(
   Component: T,
   getVariants: U
-): FC<{ variants?: VariantProps<U> } & Props<T>> => {
-  return ({ variants, className, children, ...props }) => {
-    return (
-      <Component
-        {...props}
-        className={cn(getVariants({ ...variants }), className)}
-      >
-        {children}
-      </Component>
-    );
+) => {
+  const VeComponent = Component as Resolved<T>;
+
+  return <V extends ElementType>({
+    variants,
+    className,
+    ...rest
+  }: VeProps<T, V, U>) => {
+    const props = {
+      ...rest,
+      className: cn(getVariants(variants), className),
+    } as Props<T>;
+
+    return <VeComponent<V> {...props} />;
   };
 };
-
-export default ve;
