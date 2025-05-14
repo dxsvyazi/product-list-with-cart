5d9205469aeac29ae9b3c3de181d0a26270b3468
bebra <wintersxul@gmail.com>
2025-05-15 00:04:11 +0700

    Cart items rendered via Items component

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,18 +5,26 @@ import CartItem from './CartItem';
 import Container from './base/Container';
 import Items from './Items';
 
+const EmptyCart: FC = () => (
+  <Container variants={{ flow: 'col', items: 'centered' }}>
+    <img src={empyCartIcon} />
+    <span className='text-info-secondary'>
+      Your added items will appear here
+    </span>
+  </Container>
+);
+
 const Cart: FC = () => {
   const products = useProductStore(({ products }) => products);
 
-  return products.length ? (
-    <Items items={products} to={CartItem} />
-  ) : (
-    <Container variants={{ flow: 'col', items: 'centered' }}>
-      <img src={empyCartIcon} />
-      <span className='text-info-secondary'>
-        Your added items will appear here
-      </span>
-    </Container>
+  return (
+    <Items
+      className='divide-info-primary p-0 gap-0'
+      variants={{ flow: 'col', items: 'centered', divideY: 2 }}
+      items={products}
+      to={CartItem}
+      fallback={<EmptyCart />}
+    />
   );
 };
 
