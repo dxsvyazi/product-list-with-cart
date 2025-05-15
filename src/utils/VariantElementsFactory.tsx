a903ff1f2c11f4dea336c17b5b545f7b1b0a91f6
bebra <wintersxul@gmail.com>
2025-05-15 18:32:26 +0700

    Types fixed

--- src/utils/VariantElementsFactory.tsx
+++ src/utils/VariantElementsFactory.tsx
@@ -22,14 +22,28 @@ type ResolvedProps<
   V extends ElementType
 > = T extends TComponentType ? Props<TComponentTypeG<V>> : Props<T>;
 
+type StrictExtends<T1, T2, F> = [T1] extends [T2]
+  ? [T2] extends [T1]
+    ? F
+    : T1
+  : F;
+
+export type TC<T extends ElementType, F extends ElementType> = StrictExtends<
+  T,
+  ElementType,
+  F
+>;
+
 export type TComponentType = typeof TComponent;
 
 export type TComponentTypeG<T extends ElementType> = typeof TComponent<T>;
 
-export const TComponent = <T extends ElementType>({
+export const TComponent = <T extends ElementType = 'div'>({
   as: Component = 'div' as T,
   ...props
-}: { as?: T } & Omit<Props<T>, 'as'>) => <Component {...(props as Props<T>)} />;
+}: { as?: T } & Omit<Props<TC<T, 'div'>>, 'as'>) => (
+  <Component {...(props as Props<T>)} />
+);
 
 export const ve = <T extends ElementType, U extends VeFnType>(
   Component: T,
